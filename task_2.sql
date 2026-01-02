464f1a588371a77ee3262e79346eb1f4c93712e1
Emmanuel <eabyomi028@gmail.com>
2026-01-02 16:55:25 +0100

    Add task-sql - Create all database tables

---
+++ task_2.sql
@@ -0,0 +1,41 @@
+-- Create Authors table
+CREATE TABLE IF NOT EXISTS authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Create Customers table
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT NOT NULL
+);
+
+-- Create Books table
+CREATE TABLE IF NOT EXISTS books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+-- Create Orders table
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- Create Order_Details table
+CREATE TABLE IF NOT EXISTS order_details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+);

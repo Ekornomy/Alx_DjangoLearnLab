3313892a5861fb66ddf6a90c19f106b7de7bd53c
Emmanuel <eabyomi028@gmail.com>
2026-02-17 08:01:53 +0100

    social media api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 from .models import CustomUser
 
@@ -7,35 +8,29 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
-    followers_count = serializers.IntegerField(source='followers.count', read_only=True)
-    following_count = serializers.IntegerField(source='following.count', read_only=True)
-    
     class Meta:
         model = CustomUser
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 
-                  'bio', 'profile_picture', 'followers_count', 'following_count')
-        read_only_fields = ('id',)
+                  'bio', 'profile_picture')
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    token = serializers.CharField(read_only=True)
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
     
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password', 'password2', 'first_name', 
-                  'last_name', 'bio', 'profile_picture', 'token')
+                  'last_name', 'bio')
     
     def validate(self, data):
         if data['password'] != data['password2']:
-            raise serializers.ValidationError({"password": "Passwords must match"})
+            raise serializers.ValidationError("Passwords must match")
         return data
     
     def create(self, validated_data):
         validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
-        # Create token for the user
+        user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
 
@@ -43,8 +38,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=False)
     email = serializers.EmailField(required=False)
-    password = serializers.CharField(style={'input_type': 'password'})
-    token = serializers.CharField(read_only=True)
+    password = serializers.CharField()
     
     def validate(self, data):
         username = data.get('username')
@@ -66,8 +60,5 @@ class LoginSerializer(serializers.Serializer):
         if not user:
             raise serializers.ValidationError("Invalid credentials")
         
-        # Get or create token
-        token, created = Token.objects.get_or_create(user=user)
-        data['token'] = token.key
         data['user'] = user
         return data
